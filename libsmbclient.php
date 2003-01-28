c09e62d14e7646a7ab3059e35a0bf52431628167
Matthew Sachs <matthewg@zevils.com>
2003-01-28 20:51:45 +0000

    opendir/readdir/closedir functional and tested

--- libsmbclient.php
+++ libsmbclient.php
@@ -1,9 +1,30 @@
+<html>
+<head><title>libsmbclient PHP test</title></head>
+<body>
+
+<?
+	$url = "smb://guest:guest@minusone/";
+	if(isset($_REQUEST["url"])) $url = $_REQUEST["url"];
+?>
+
+<h1>libsmbclient PHP test</h1>
+<form method="post" action="libsmbclient.php">
+URL: <input name="url" type="text" value="<?echo htmlentities($url)?>" size="100"><br>
+<input type="submit" name="submit" value="Browse">
+</form>
+<p>
+<tt>
 <?php
 //dl("./.libs/libsmbclient.so");
 
-$param = 2;
-$return = smbclient_test($param);
-
-print("We sent \"$param\" and got \"$return\"");
-
+if(isset($_REQUEST["submit"])) {
+	$dh = smbclient_opendir($url);
+	while($de = smbclient_readdir($dh)) {
+		printf("%s: %s (%s)<br>\n", $de["type"], $de["name"], $de["comment"]);
+	}
+	smbclient_closedir($dh);
+}
 ?>
+</tt>
+</p>
+</body></html>
