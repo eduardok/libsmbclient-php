ce81c21f8fe2e6385c83c8d47647103a24a13f5c
Alfred Klomp <git@alfredklomp.com>
2014-08-09 21:08:20 +0200

    smbclient_write: add integration tests

---
+++ tests/WriteTest.php
@@ -0,0 +1,74 @@
+<?php
+
+class WriteTest extends PHPUnit_Framework_TestCase
+{
+	private $testdata =
+		"Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n";
+
+	// The URI of the test file seen through Samba:
+	private $testuri = 'smb://localhost/testshare/writetest.txt';
+
+	// The "real" file on the filesystem:
+	private $realfile = '/home/testuser/testshare/writetest.txt';
+
+	public function
+	testWriteSuccess ()
+	{
+		$state = smbclient_state_new();
+		smbclient_state_init($state, null, 'testuser', 'password');
+		$file = smbclient_creat($state, $this->testuri);
+		$this->assertTrue(is_resource($file));
+
+		$ret = smbclient_write($state, $file, $this->testdata);
+		$this->assertTrue(is_integer($ret));
+
+		$this->assertEquals(strlen($this->testdata), $ret);
+
+		// Use the regular filesystem to check the file that was created:
+		$this->assertFileExists($this->realfile);
+		$this->assertEquals(filesize($this->realfile), $ret);
+
+		$this->assertStringEqualsFile($this->realfile, $this->testdata);
+	}
+
+	public function
+	testWritePartial ()
+	{
+		$state = smbclient_state_new();
+		smbclient_state_init($state, null, 'testuser', 'password');
+		$file = smbclient_creat($state, $this->testuri);
+		$this->assertTrue(is_resource($file));
+
+		// Write just 30 bytes:
+		$ret = smbclient_write($state, $file, $this->testdata, 30);
+		$this->assertEquals(30, $ret);
+
+		// Use the regular filesystem to check the file that was created:
+		$this->assertFileExists($this->realfile);
+		$this->assertEquals(filesize($this->realfile), $ret);
+
+		$this->assertStringEqualsFile($this->realfile, substr($this->testdata, 0, 30));
+	}
+
+	public function
+	testWriteOversized ()
+	{
+		$state = smbclient_state_new();
+		smbclient_state_init($state, null, 'testuser', 'password');
+		$file = smbclient_creat($state, $this->testuri);
+		$this->assertTrue(is_resource($file));
+
+		// Write way more bytes than data:
+		$ret = smbclient_write($state, $file, $this->testdata, 1000);
+		$this->assertTrue(is_integer($ret));
+
+		$this->assertEquals(strlen($this->testdata), $ret);
+
+		// Use the regular filesystem to check the file that was created:
+		$this->assertFileExists($this->realfile);
+		$this->assertEquals(filesize($this->realfile), $ret);
+
+		$this->assertStringEqualsFile($this->realfile, $this->testdata);
+	}
+}
