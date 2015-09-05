0f4a3716223ec9bbadece414e5d6b51659ab891e
Remi Collet <fedora@famillecollet.com>
2015-09-05 19:20:16 +0200

    credentials can be part of the URI

--- tests/WriteTest.php
+++ tests/WriteTest.php
@@ -12,17 +12,48 @@ class WriteTest extends PHPUnit_Framework_TestCase
 	// The "real" file on the filesystem:
 	private $realfile;
 
-	public function setup() {
+	public function
+	setup()
+	{
 		$this->testuri = 'smb://'.SMB_HOST.'/'.SMB_SHARE.'/writetest.txt';
 		$this->realfile = SMB_LOCAL.'/writetest.txt';
+		$this->testuri2 = 'smb://'.SMB_USER.':'.SMB_PASS.'@'.SMB_HOST.'/'.SMB_SHARE.'/writetest.txt';
+	}
+
+	public function
+	tearDown()
+	{
+		@unlink($this->realfile);
 	}
 
 	public function
 	testWriteSuccess ()
 	{
+		/* credentials sent during init */
 		$state = smbclient_state_new();
 		smbclient_state_init($state, null, SMB_USER, SMB_PASS);
 		$file = smbclient_creat($state, $this->testuri);
+		$this->assertTrue(is_resource($file));
+
+		$ret = smbclient_write($state, $file, $this->testdata);
+		$this->assertTrue(is_integer($ret));
+
+		$this->assertEquals(strlen($this->testdata), $ret);
+
+		// Use the regular filesystem to check the file that was created:
+		$this->assertFileExists($this->realfile);
+		$this->assertEquals(filesize($this->realfile), $ret);
+
+		$this->assertStringEqualsFile($this->realfile, $this->testdata);
+	}
+
+	public function
+	testWriteSuccess2 ()
+	{
+		/* credentials in URI */
+		$state = smbclient_state_new();
+		smbclient_state_init($state);
+		$file = smbclient_creat($state, $this->testuri2);
 		$this->assertTrue(is_resource($file));
 
 		$ret = smbclient_write($state, $file, $this->testdata);
