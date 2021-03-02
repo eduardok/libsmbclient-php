c3419b4b87d7cbfd3c6f142fd531b7451b09e91d
Eduardo Kienetz <eduardo@kienetz.com>
2021-03-01 22:33:05 -0500

    Revert "Test suspicion"
    This reverts commit 09c155d9a8b3e6c61391fa15284dfd00f6514330.
    Nevermind, it was correct, heh.

--- tests/ClosedirTest.php
+++ tests/ClosedirTest.php
@@ -18,6 +18,8 @@ final class ClosedirTest extends TestCase
 	public function
 	testClosedirEmpty ()
 	{
+		$state = smbclient_state_new();
+		smbclient_state_init($state, null, SMB_USER, SMB_PASS);
 		$this->assertFalse(smbclient_closedir($state));
 	}
 
@@ -27,6 +29,8 @@ final class ClosedirTest extends TestCase
 	public function
 	testClosedirNull ()
 	{
+		$state = smbclient_state_new();
+		smbclient_state_init($state, null, SMB_USER, SMB_PASS);
 		$this->assertTrue(smbclient_closedir($state, null));
 	}
 
@@ -36,6 +40,8 @@ final class ClosedirTest extends TestCase
 	public function
 	testClosedirDouble ()
 	{
+		$state = smbclient_state_new();
+		smbclient_state_init($state, null, SMB_USER, SMB_PASS);
 		$dir = smbclient_opendir($state, 'smb://'.SMB_HOST.'/'.SMB_SHARE);
 		$this->assertTrue(smbclient_closedir($state, $dir));
 		$this->assertFalse(smbclient_closedir($state, $dir));
