ff30047c900e0cc8965138c3bba0b86095dea5bb
Eduardo Kienetz <eduardo@kienetz.com>
2023-04-01 17:23:17 -0400

    Prepare release 1.1.0

--- php_smbclient.h
+++ php_smbclient.h
@@ -43,7 +43,7 @@
 
 #include <libsmbclient.h>
 
-#define PHP_SMBCLIENT_VERSION "1.0.7"
+#define PHP_SMBCLIENT_VERSION "1.1.0"
 
 extern zend_module_entry smbclient_module_entry;
 #define phpext_smbclient_ptr &smbclient_module_entry
